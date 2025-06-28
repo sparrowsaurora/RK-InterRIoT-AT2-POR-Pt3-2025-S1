47818afdb4c0446792aa9ce6989c049389f51369
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2025-06-28 16:21:53 +0800

    feat(step-8): password validation

--- api.py
+++ api.py
@@ -15,13 +15,14 @@
 from pathlib import Path
 
 from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 import casbin
 from fastapi_authz import CasbinMiddleware
 from starlette.middleware.authentication import AuthenticationMiddleware
+from starlette.authentication import requires, AuthenticationError
 
 import basic_auth as basic_auth
 
@@ -71,9 +72,23 @@ async def home(request: Request):
         name="pages/home.html"
     )
 
+@api.exception_handler(AuthenticationError)
+async def auth_exception_handler(request: Request, exc: AuthenticationError):
+    '''Custom authentication error handler to return JSON response'''
+    return JSONResponse(
+        content={"success": False, "message": str(exc), "data": []},
+        status_code=401
+    )
 @api.get("/api")
 async def api_index():
-    return '{"success":true, "message": "Welcome", "data": ["greeting": "Hello, world."]}'
+    return JSONResponse(content={
+    "success": True,
+    "message": "Endpoint: /",
+    "data": [
+        {"greeting": "Hello, world."}
+    ]
+})
+
 
 @api.get("/about", response_class=HTMLResponse)
 async def about(request: Request):
@@ -88,10 +103,24 @@ async def api_data(request: Request):
 
 @api.get("/api/ds1/res1 ")
 async def api_ds1(request: Request):
-    return "ds1/res1"
+    return JSONResponse(content={
+    "success": True,
+    "message": "Endpoint: ds1/res1",
+    "data": [
+        {"greeting": "Resource 1 accessed"}
+    ]
+})
+
 
 @api.get("/api/ds2/res2 ")
 async def api_ds2(request: Request):
-    return "ds2/res2"
+    return JSONResponse(content={
+    "success": True,
+    "message": "Endpoint: ds2/res2",
+    "data": [
+        {"greeting": "Resource 2 accessed"}
+    ]
+})
+
 
 # Main Code
